796cc2089b2165d02ceea033cbf05efe2724cb4f
LinoM0 <lino.mueller@hotmail.com>
2025-08-29 11:26:11 +0200

    Added Ledger, Person and Expense classes

--- project.py
+++ project.py
@@ -1,13 +1,46 @@
-def main(): ...
+class Ledger:
+    def __init__(self, people=[], expenses=[]):
+        self.people = people
+        self.expenses = expenses
+
+    def add_person(self, name):
+        person = Person(name)
+        self.people.append(person)
+
+    def add_expense(self, payer, amount):
+        expense = Expense(payer, amount)
+        self.expenses.append(expense)
+
+    def __str__(self):
+        return f"{self.people} and {self.expenses}"
 
 
-def function_1(): ...
+class Person:
+    def __init__(self, name):
+        self.name = name
 
 
-def function_2(): ...
+class Expense:
+    def __init__(self, payer, amount):
+        self.payer = payer
+        self.amount = amount
 
 
-def function_n(): ...
+def main():
+    ledger = Ledger()
+    ledger.add_person("Lino")
+    ledger.add_person("Victoria")
+    ledger.add_expense("Lino", 10)
+    print(ledger)
+
+
+# def function_1(): ...
+
+
+# def function_2(): ...
+
+
+# def function_n(): ...
 
 
 if __name__ == "__main__":
