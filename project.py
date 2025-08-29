d119829ed609346e6b31bb1b02c36720501a1d58
LinoM0 <lino.mueller@hotmail.com>
2025-08-29 12:52:19 +0200

    Added properties for Person and Expense class

--- project.py
+++ project.py
@@ -3,8 +3,8 @@ class Ledger:
         self.people = people
         self.expenses = expenses
 
-    def add_person(self, name):
-        person = Person(name)
+    def add_person(self, name, balance=0):
+        person = Person(name, balance)
         self.people[name] = person
 
     def add_expense(self, payer, amount):
@@ -20,19 +20,35 @@ class Ledger:
             person.balance -= total / len(self.people.keys())
 
     def __str__(self):
-        return (
-            f"People:\n"
-            f"{self.people['Lino']}\n"
-            f"{self.people['Victoria']}\n\n"
-            f"Expenses:\n"
-            f"{self.expenses[0]}"
-        )
+        people_str = "\n".join(str(person) for person in self.people.values())
+        expenses_str = "\n".join(str(expense) for expense in self.expenses)
+        return f"People:\n{people_str}\n\nExpenses:\n{expenses_str}"
 
 
 class Person:
-    def __init__(self, name, balance=0):
+    def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @balance.setter
+    def balance(self, balance):
+        if not isinstance(balance, (int, float)):
+            raise ValueError("Balance is not numeric")
+        self._balance = balance
 
     def __str__(self):
         return f"{self.name} with balance: {self.balance}£"
@@ -43,6 +59,26 @@ class Expense:
         self.payer = payer
         self.amount = amount
 
+    @property
+    def payer(self):
+        return self._payer
+
+    @payer.setter
+    def payer(self, payer):
+        if not payer:
+            raise ValueError("Missing payer")
+        self._payer = payer
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, amount):
+        if not isinstance(amount, (int, float)):
+            raise ValueError("Expense is not numeric")
+        self._amount = amount
+
     def __str__(self):
         return f"{self.amount}£ from {self.payer}"
 
@@ -51,7 +87,10 @@ def main():
     ledger = Ledger()
     ledger.add_person("Lino")
     ledger.add_person("Victoria")
+    ledger.add_person("Bella", -100)
     ledger.add_expense("Lino", 10)
+    ledger.add_expense("Victoria", 20)
+    ledger.add_expense("Lino", 5)
     print(ledger)
     ledger.split()
     print(ledger)
