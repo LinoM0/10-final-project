dce2e748e471db00fa060e0d71840c7e0d1ea5bb
LinoM0 <lino.mueller@hotmail.com>
2025-08-29 12:15:15 +0200

    Added equal split functionality, connected expenses to person

--- project.py
+++ project.py
@@ -1,23 +1,41 @@
 class Ledger:
-    def __init__(self, people=[], expenses=[]):
+    def __init__(self, people={}, expenses=[]):
         self.people = people
         self.expenses = expenses
 
     def add_person(self, name):
         person = Person(name)
-        self.people.append(person)
+        self.people[name] = person
 
     def add_expense(self, payer, amount):
         expense = Expense(payer, amount)
+        self.people[f"{payer}"].balance += amount
         self.expenses.append(expense)
 
+    def split(self):
+        total = 0
+        for expense in self.expenses:
+            total += expense.amount
+        for person in self.people.values():
+            person.balance -= total / len(self.people.keys())
+
     def __str__(self):
-        return f"{self.people} and {self.expenses}"
+        return (
+            f"People:\n"
+            f"{self.people['Lino']}\n"
+            f"{self.people['Victoria']}\n\n"
+            f"Expenses:\n"
+            f"{self.expenses[0]}"
+        )
 
 
 class Person:
-    def __init__(self, name):
+    def __init__(self, name, balance=0):
         self.name = name
+        self.balance = balance
+
+    def __str__(self):
+        return f"{self.name} with balance: {self.balance}£"
 
 
 class Expense:
@@ -25,12 +43,17 @@ class Expense:
         self.payer = payer
         self.amount = amount
 
+    def __str__(self):
+        return f"{self.amount}£ from {self.payer}"
+
 
 def main():
     ledger = Ledger()
     ledger.add_person("Lino")
     ledger.add_person("Victoria")
     ledger.add_expense("Lino", 10)
+    print(ledger)
+    ledger.split()
     print(ledger)
 
 
